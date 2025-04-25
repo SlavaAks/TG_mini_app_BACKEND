10d03d4df1556fba58dc23262a27abd4e181992d
SLAVA <aks8slavent@gmail.com>
2025-04-25 15:13:24 +0000

    .env

--- config_reader.py
+++ config_reader.py
@@ -15,8 +15,8 @@ class Config(BaseSettings):
     MANAGER_ID: List[int]
     CHANEL_ID: SecretStr
 
-    WEBHOOK_URL: str = "https://solve-watershed-font-regards.trycloudflare.com"
-    WEBAPP_URL: str = "https://continent-bytes-florist-sell.trycloudflare.com"
+    WEBHOOK_URL: str = "https://lawkashop.space"
+    WEBAPP_URL: str = "https://lawkashop.space"
 
     APP_HOST: str = "localhost"
     APP_PORT: int = 8080
